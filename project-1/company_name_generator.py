3b773dfa148d24a5c55ef1f01d5db0713483b70d
Ali <ramezankhani.ali@gmail.com>
2022-09-10 13:12:48 +0430

    Complete first project

---
+++ project-1/company_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Company Name Generator.")
+name = input("What's your name?\n")
+industry = input("Please input your industry:\n")
+print("Your company name could be: " + name + " " + industry)
